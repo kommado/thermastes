66f3302c1f0f7b9822682ed6f00ca1d9cbfc84e6
band songs <norely@band.songs>
2025-10-07 14:47:18 +0000

    Saving new songs

--- update.sh
+++ update.sh
@@ -2,5 +2,14 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR
-git pull --rebase
+if ! git diff --quiet || ! git diff --cached --quiet || [ -n "$(git ls-files --others --exclude-standard)" ]; then
+  echo "Local changes exist"
+  git add --all
+  git commit -m "Saving new songs"
+  git pull --rebase
+  git push origin master
+else
+  echo "Working tree clean"
+  git pull --rebase
+fi
 popd
